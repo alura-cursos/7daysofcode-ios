481e4caf320c31346f66df6c602019c6efad2563
Giovanna <giovannagmoeller@gmail.com>
2022-10-25 01:05:51 -0300

    alterando modelo de dados para receber dados da API

--- MovieList/Model/Movie.swift
+++ MovieList/Model/Movie.swift
@@ -7,18 +7,24 @@
 
 import Foundation
 
-struct Movie {
+struct MovieResults: Codable {
+    let results: [Movie]
+}
+
+struct Movie: Codable {
     let id: Int
     let title: String
     let releaseDate: String
     let image: String?
     let overview: String
     let voteAverage: Double
+    
+    private enum CodingKeys: String, CodingKey {
+        case id
+        case title
+        case releaseDate = "release_date"
+        case image = "poster_path"
+        case overview
+        case voteAverage = "vote_average"
+    }
 }
-
-let movies: [Movie] = [
-    Movie(id: 1, title: "Órfã 2: A Origem", releaseDate: "2022-07-27", image: nil, overview: "", voteAverage: 7.2),
-    Movie(id: 2, title: "Minions 2: A Origem de Gru", releaseDate: "2022-06-29", image: nil, overview: "", voteAverage: 7.8),
-    Movie(id: 3, title: "Thor: Amor e Trovão", releaseDate: "2022-07-06", image: nil, overview: "", voteAverage: 6.8),
-    Movie(id: 4, title: "Avatar", releaseDate: "2009-12-18", image: nil, overview: "", voteAverage: 8.8),
-]
