69e6fc609a5d111d100d766f7d03b0342743c099
Giovanna <giovannagmoeller@gmail.com>
2022-10-25 00:52:35 -0300

    criando extensão para adicionar cores

---
+++ MovieList/Extensions/UIColor+.swift
@@ -0,0 +1,14 @@
+//
+//  UIColor+.swift
+//  MovieList
+//
+//  Created by Giovanna Moeller on 25/10/22.
+//
+
+import Foundation
+import UIKit
+
+extension UIColor {
+    static let lightBackground = UIColor(red: 46/255, green: 19/255, blue: 113/255, alpha: 1.0)
+    static let darkBackground = UIColor(red: 19/255, green: 11/255, blue: 43/255, alpha: 1.0)
+}
