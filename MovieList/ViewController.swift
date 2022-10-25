eedd33d6165507459e8a776dfd7e8d5616318b9d
Giovanna <giovannagmoeller@gmail.com>
2022-10-25 00:47:57 -0300

    first commit

---
+++ MovieList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MovieList
+//
+//  Created by Giovanna Moeller on 25/10/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
